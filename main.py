47283e0a313fac2cc511fe847f66d30971baf1a6
seanmurphy611 <spm6407@psu.edu>
2020-09-17 19:58:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Sean Murphy spm6407@psu.edu
+# Yon Lin Kwang yqk5211@psu.edu 
+# Aidan Mayo ajm8132@psu.edu 
+#Section 1
+#Breakout 8
+
+def sum_n(n):
+  if n<=1:
+    return 1
+  else:
+    return n + sum_n(n-1)
+
+def print_n(s,n):
+  if n<=1:
+    print(s)
+  else:
+    print(s)
+    print_n(s,n-1)
+
+def run():
+  n = int(input("Enter an int: "))
+  out = sum_n(n)
+  print("sum is: " + str(out)+ ".")
+  s = input("Enter a string: ")
+  print_n(s,n)
+
+if __name__ == "__main__":
+  run()
